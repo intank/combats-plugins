b38a770c39662ed9be1fc913153c7bcb029181c9
shegeda <shegeda@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2007-11-30 18:33:19 +0000

    Chat_filter splittet by two. Filter is just engine. Hilight is event handler

--- chat_filter/plugin.js
+++ chat_filter/plugin.js
@@ -1,32 +1,6 @@
 (function() {
   function plugin_chat_filter() {
     this.Init();
-    var clear = function(match,eventObj) {
-//      alert('clear: '+match[0]);
-      eventObj.mess = '';
-    }
-    var mark = function(match,eventObj) {
-//      alert('mark: '+match[0]);
-      eventObj.mess = 
-        eventObj.mess.substr(0,match.index)+
-        '<font style="background:yellow">'+
-        match[0]+
-        '</font>'+
-        eventObj.mess.substr(match.lastIndex);
-      var volume=top.frames['bottom'].soundvol;
-      top.frames['bottom'].window.document.Sound.SetVariable("Volume", (volume*50));
-      top.frames['bottom'].window.document.Sound.SetVariable("Sndplay",1);
-    }
-    this.addFilter(
-      { filter:/<font class=sysdate>\d+\:\d+<\/font>\s*(?:Вас приветствует\:  <SPAN>.*?<\/SPAN>|\[<SPAN>.*?<\/SPAN>\] приветствует Вас|\[<SPAN>.*?<\/SPAN>\] отправил(?:ся|ась) )/i,
-        handler:clear
-      }
-    );
-    this.addFilter(
-      { filter:/((?:нуж.{2,4}|пр[оа]да(?:[сеё]т|й)|[^>\s]*куп.{2,6}|(?:вы|при)(н[еио]с(?:[иеё]т|ти))|кто)\s+(?:хав[кч]\S+|корм|еду|еда|(свит\S+)?\s*(?:жж\S+|холодн\S+|напад\S+|цеп\S+|сокр\S*)))/i,
-        handler:mark
-      }
-    );
   }
 
   plugin_chat_filter.prototype = {
