970e0c91aaf6ffd82c33f8766fd8c5d9da43a067
shegeda <shegeda@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2007-12-02 06:55:31 +0000

    Chat_filter splittet by two. Filter is just engine. Hilight is event handler

---
+++ trunk/chat_hilight/plugin.js
@@ -0,0 +1,76 @@
+(function(){
+  plugin_hilight = function(){
+    this.setProperties([
+      { value: this.load('clearRegexp','') },
+      { value: this.load('markRegexp','') }
+    ]);
+  }
+
+  plugin_hilight.prototype = {
+    clearRegexp: /^$/, // /<font class=sysdate>\d+\:\d+<\/font>\s*(?:Вас приветствует\:  <SPAN>.*?<\/SPAN>|\[<SPAN>.*?<\/SPAN>\] приветствует Вас|\[<SPAN>.*?<\/SPAN>\] отправил(?:ся|ась) )/i,
+    markRegexp: /^$/, // /((?:нуж.{2,4}|пр[оа]да(?:[сеё]т|й)|[^>\s]*куп.{2,6}|(?:вы|при)(н[еио]с(?:[иеё]т|ти))|кто)\s+(?:хав[кч]\S+|корм|еду|еда|(свит\S+)?\s*(?:жж\S+|холодн\S+|напад\S+|цеп\S+|сокр\S*)))/i,
+    clearFilterId: '',
+    markFilterId: '',
+    toString: function() {
+      return "Подсветка и удаление сообщений";
+    },
+    getProperties: function() {
+      return [
+        { name: "RexExp удаления", value: this.clearRegexp.source },
+        { name: "RexExp выделения", value: this.markRegexp.source }
+      ];
+    },
+    setProperties: function(a) {
+      try {
+        this.clearRegexp = this.clearRegexp.compile(a[0].value,'i');
+        this.markRegexp = this.markRegexp.compile(a[1].value,'i');
+        this.loadFilters();
+        this.save('clearRegexp',a[0].value);
+        this.save('markRegexp',a[1].value);
+      } catch (e) {
+        alert('Ошибка настройки фильтров: "'+e.message+'"');
+      }
+    },
+    load: function(key,def_val){
+      return external.m2_readIni(combats_plugins_manager.security_id,"Combats.RU","chat_hilight\\settings.ini",top.getCookie('battle'),key,def_val);
+    },
+    save: function(key,val){
+      external.m2_writeIni(combats_plugins_manager.security_id,"Combats.RU","chat_hilight\\settings.ini",top.getCookie('battle'),key,val);
+    },
+    loadFilters: function() {
+      var filterPlugin = combats_plugins_manager.plugins_list['chat_filter'];
+      if (!filterPlugin) {
+        throw new Exception('Для корретной работы нужен плагин chat_filter');
+      }
+      if (this.clearFilterId!='')
+        filterPlugin.removeFilter(this.clearFilterId);
+      this.clearFilterId = filterPlugin.addFilter(
+        { filter:this.clearRegexp,
+          handler:this.clear
+        }
+      );
+      if (this.markFilterId!='')
+        filterPlugin.removeFilter(this.clearFilterId);
+      this.markFilterId = filterPlugin.addFilter(
+        { filter:this.markRegexp,
+          handler:this.mark
+        }
+      );
+    },
+    clear: function(match,eventObj) {
+      eventObj.mess = '';
+    },
+    mark: function(match,eventObj) {
+      eventObj.mess = 
+        eventObj.mess.substr(0,match.index)+
+        '<font style="background:yellow">'+
+        match[0]+
+        '</font>'+
+        eventObj.mess.substr(match.lastIndex);
+      var volume=top.frames['bottom'].soundvol;
+      top.frames['bottom'].window.document.Sound.SetVariable("Volume", (volume*50));
+      top.frames['bottom'].window.document.Sound.SetVariable("Sndplay",1);
+    }
+  }
+  return new plugin_hilight();
+})()
