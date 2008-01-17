bac489478ce739b6ac552d84ae10e79d1bb25bb9
shegeda <shegeda@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2008-01-17 13:37:11 +0000

    [fix] Cfg panel: incorrectly worked with quotes in strings [fix] sleep: reanimated [implementation] Main module: exception handling while event processing added [implementation] chat_removeduplicates: remove old messages introduced, plugin being made configurable [implementation] fast_cast: optimization. May be unstable

--- trunk/sleep/plugin.js
+++ trunk/sleep/plugin.js
@@ -6,13 +6,33 @@
   };
 
   plugin_sleep.prototype = {
+    goingToRoom: false,
     toString: function() {
       return "Усыпальница :)";
     },
+    goToRoom: function(onclick) {
+      this.goingToRoom = true;
+      return onclick?onclick.apply(frames[3],[]):true;
+    },
     onloadHandler: function() {
-      if (frames[3].location.href.search(/^http\:\/\/\w+\.combats\.ru\/house\.pl\?.*?path=1.200.[12]/)!=0)
+      if (frames[3].location.pathname!='/house.pl')
         return;
-      top.frames[3].location = frames[3].location.protocol+'//'+frames[3].location.hostname+'/house.pl?to_sleep=1&sd4=&room=4&'+Math.random();
+      if (this.goingToRoom) {
+        this.goingToRoom = false;
+        top.frames[3].location = frames[3].location.protocol+'//'+frames[3].location.hostname+'/house.pl?to_sleep=1&sd4=&room=4&'+Math.random();
+        return;
+      } else
+        this.goingToRoom = false;
+      var moveto = frames[3].document.getElementById('moveto');
+      var directions = moveto.getElementsByTagName('a');
+      for (var i=0; i<directions.length; i++)
+        if (directions[i].search.search(/[\?&]path=1\.200.[125](?:$|&)/)>=0) {
+          directions[i].onclick = combats_plugins_manager.get_binded_method(
+            this,
+            this.goToRoom,
+            directions[i].onclick
+          );
+        }
     }
   };
   return new plugin_sleep();
