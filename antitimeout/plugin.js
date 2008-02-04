15554c23a5cafec5ddc4a6b41ce6ebe2caf7718d
soltik <soltik@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2008-02-04 18:47:44 +0000

    Обновлен после изменения боевой системы. Без кнопок, одна настройка как и была, интервал ударов. При старте боя удар не наносит, ждет заданного таймаута. При занятости очереди ударов пытается обновить и ждет 5 сек., потом снова пытается ударить итд. По завершении боя возвращает управление предыдущему экрану (тычет "Вернуться").

--- antitimeout/plugin.js
+++ antitimeout/plugin.js
@@ -1,82 +1,84 @@
-function plugin_antitimeout() {
-  this.load = function(key,def_val){
-    return external.m2_readIni(combats_plugins_manager.security_id,"Combats.RU","antitimeout\\antitimeout.ini",top.getCookie('battle'),key,def_val);
-  };
-  this.save = function(key,val){
-    external.m2_writeIni(combats_plugins_manager.security_id,"Combats.RU","antitimeout\\antitimeout.ini",top.getCookie('battle'),key,val);
-  }
-  this.autotime = parseInt(this.load("autotime","90")); // время в секундах до самостоятельного удара
-  this.kickTimer = null;
-  this.toString = function() {
-    return "Предотвращение тайм-аута (автобой)";
-  }
-}
+(function() {
+	plugin_antitimeout = function() {
+		this.autotime = 0;
+		this.kickTimer = null;
+		this.init();
+		this.onloadHandler();
+	}
 
-plugin_antitimeout.prototype.getProperties = function() {
-  return [
-    { name:"Время автоудара (сек)", value: this.autotime }
-  ];
-}
+	plugin_antitimeout.prototype.toString = function() {
+		return "Предотвращение тайм-аута (автобой)"; 
+	}
 
-plugin_antitimeout.prototype.setProperties = function(a) {
-  this.autotime=a[0].value;
-  this.save("autotime",this.autotime); // время в секундах до самостоятельного удара
-}
+	plugin_antitimeout.prototype.getProperties = function() {
+		return [{ name:"Время автоудара (сек)", value: this.autotime }];
+	}
+	
+	plugin_antitimeout.prototype.setProperties = function(a) {
+		this.autotime=a[0].value;
+		external.m2_writeIni(top.combats_plugins_manager.security_id,"Combats.RU","antitimeout\\antitimeout.ini",top.getCookie('battle'),"autotime",this.autotime);
+	}
+	
+	plugin_antitimeout.prototype.clearKickTimer = function() {
+		if (this.kickTimer==null)
+			return;
+		clearTimeout(this.kickTimer);
+		this.kickTimer = null;
+	}
+	
+	plugin_antitimeout.prototype.setKickTimer = function(kick_time) {
+		if (this.kickTimer!=null)
+			clearTimeout(this.kickTimer);
+		this.kickTimer = setTimeout( top.combats_plugins_manager.get_binded_method(this,this.autoKick), kick_time);
+	}
 
-plugin_antitimeout.prototype.clearKickTimer = function() {
-  if (antitimeout.kickTimer==null)
-    return;
-  clearTimeout(antitimeout.kickTimer);
-  antitimeout.kickTimer = null;
-}
+	plugin_antitimeout.prototype.autoKick = function() {
+		try {
+			if( (typeof(top.Battle.oBattle )!="object") && !top.Battle.bInBattle) 
+				return;
+			if(top.Battle.oBattle.bGameOver){ //---------------- Гейм оувер
+				top.Battle.End(top.Battle.oBattle.sLocation);
+				return;
+			}
+			if( top.Battle.oBattle.nRequests ) { //------------------- Если занято, обновляем и ждем 5 сек
+				top.Chat.am('Busy! '+top.Battle.oBattle.nRequests); 
+				top.Battle.oBattle.Refresh();
+				top.Battle.oBattle.nRequests=0;
+				this.setKickTimer(3*1000);
+			}
 
-plugin_antitimeout.prototype.autoKick = function() {
-  elements = top.frames[3].document.forms["f1"].elements;
-  var block = Math.floor(Math.random()*5);
-  var is_block=true;
-  var point=-1;
-  if ("let_attack" in elements) {
-    for (i=5; i>=0; i--) {
-      if (is_block) {
-        s = "r_"+i+"_"+block;
-        if (s in elements)
-          is_block = false;
-      } else {
-        do {
-          new_point=(block+Math.floor(Math.random()*3))%5;
-        } while (new_point==point);
-        point=new_point;
-        s = "r_"+i+"_"+point;
-      }
-      if (s in elements) {
-        elements[s].click();
-      }
-    }
-    elements["let_attack"].click();
-  } else
-    top.frames[3].location = top.frames[3].location.href;
-}
+			top.Battle.oBattle.Attack(); //------------ Наносим удар
+			this.setKickTimer(this.autotime*1000);
+			//top.Battle.oBattle.Attack();
+		}catch(e){
+			e.Function = 'autoKick';
+        	combats_plugins_manager.logError(this,e);
+		}
+	}
+	
+	plugin_antitimeout.prototype.onloadHandler = function() {
+		try {
+			if (top.frames[3].location.href.search(/^http\:\/\/\w+\.combats\.ru\/battle\d*\.pl/)!=0)
+				return;
+			if (this.autotime>0)
+				this.setKickTimer(this.autotime*1000);
+			else
+				this.kickTimer = null;
+			top.frames[3].attachEvent( "onbeforeunload", top.combats_plugins_manager.get_binded_method(this,this.clearKickTimer));
+			//  d = top.Battle.oBattle.oLayer.ownerDocument;
+  
+		}catch(e){
+			e.Function = 'onLoadHandler';
+        	combats_plugins_manager.logError(this,e);
+		}
+	}
+	
+	plugin_antitimeout.prototype.init = function() {
+		var t = external.m2_readIni(top.combats_plugins_manager.security_id,"Combats.RU","antitimeout\\antitimeout.ini",top.getCookie('battle'),"autotime","90");
+		this.autotime = parseInt(t); // время в секундах до самостоятельного удара
 
-plugin_antitimeout.prototype.onloadHandler = function() {
-  if (top.frames[3].location.href.search(/^http\:\/\/\w+\.combats\.ru\/battle\d*\.pl/)!=0)
-    return;
-
-  if (antitimeout.kickTimer!=null) 
-    clearTimeout(antitimeout.kickTimer);
-  if (antitimeout.autotime>0)
-    antitimeout.kickTimer = setTimeout( antitimeout.autoKick, antitimeout.autotime*1000 );
-  else
-    antitimeout.kickTimer = null;
-  top.frames[3].attachEvent( "onbeforeunload", antitimeout.clearKickTimer);
-  d = top.frames[3].document;
-  if (("f1" in d.forms) && ("let_attack" in d.forms["f1"].elements)) {
-    button = d.createElement("<input type=button value='Случайный Удар'>");
-    attack_button = d.forms["f1"].elements["let_attack"];
-    attack_button.parentNode.insertBefore(button,attack_button);
-    button.attachEvent("onclick", antitimeout.autoKick);
-  }
-}
-
-antitimeout = new plugin_antitimeout();
-top.frames[3].frameElement.attachEvent("onload",function(){antitimeout.onloadHandler();});
-antitimeout
+		top.combats_plugins_manager.attachEvent('mainframe.load',top.combats_plugins_manager.get_binded_method(this,this.onloadHandler));
+	}
+	
+	return new plugin_antitimeout();
+})()
