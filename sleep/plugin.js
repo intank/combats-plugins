40bcf76b887d9a08533b42cd681f18ce15b24697
shegeda <shegeda@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2008-10-09 07:02:06 +0000

    sleep: Character can wait for weakness gone before go to sleep

--- sleep/plugin.js
+++ sleep/plugin.js
@@ -7,15 +7,46 @@
 
   plugin_sleep.prototype = {
     goingToRoom: false,
+    timerId: null,
+    req: null,
+    waitForWeakeningGone: false,
     toString: function() {
       return "Усыпальница :)";
+    },
+    getProperties: function() {
+      return [{name:'Дождаться завершения ослабления', value:this.waitForWeakeningGone}];
+    },
+    setProperties: function(a) {
+      this.waitForWeakeningGone = a[0].value;
     },
     goToRoom: function(onclick) {
       this.goingToRoom = true;
       return onclick?onclick.apply(frames[3],[]):true;
     },
+    sleep: function() {
+      var d = top.combats_plugins_manager.getMainFrame().document;
+      if (this.waitForWeakeningGone) {
+        if (this.timerId) {
+          clearTimeout(this.timerId);
+        }
+        if (!this.req)
+          this.req = top.combats_plugins_manager.getHTTPRequest();
+        this.req.open('GET', '/main.pl?edit=5&'+Math.random(), false);
+        this.req.send('');
+        s = this.req.responseText;
+        var match = s.match(/<B><U>Ослабление после боя<\/U><\/B>\s*\(Эффект\)<BR>Осталось\:\s*(\d+)\s*мин\.<BR>/);
+        if (match) {
+          this.timerId = setTimeout(
+            top.combats_plugins_manager.get_binded_method(this, this.sleep), 
+            60*parseFloat(match[1])*1000
+          );
+          return;
+        }
+      }
+      d.location = d.location.protocol+'//'+d.location.hostname+'/house.pl?to_sleep=1&sd4=&room=4&'+Math.random();
+    },
     onloadHandler: function() {
-      if (frames[3].location.pathname!='/house.pl')
+      if (top.combats_plugins_manager.getMainFrame().location.pathname!='/house.pl')
         return;
       var d = top.combats_plugins_manager.getMainFrame().document;
       if (this.goingToRoom) {
@@ -29,7 +60,7 @@
           }
         }
         if (canSleep) {
-          top.frames[3].location = d.location.protocol+'//'+d.location.hostname+'/house.pl?to_sleep=1&sd4=&room=4&'+Math.random();
+          this.sleep();
           return;
         }
       } else
