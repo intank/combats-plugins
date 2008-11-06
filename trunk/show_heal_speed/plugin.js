27d8a6bffd9d47c9623d5e124a921ee73dcac658
shegeda@ukr.net <shegeda@ukr.net@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2008-11-06 19:08:16 +0000

    calculation of mana restoration speed was added

--- trunk/show_heal_speed/plugin.js
+++ trunk/show_heal_speed/plugin.js
@@ -10,4 +10,15 @@
         HP.innerHTML = HP.innerHTML+'(100HP/'+T+'сек.)';
     }
   }
+  var old_setManalocal = top.setManalocal;
+  top.setManalocal = function(){
+    old_setManalocal.apply(top,[]);
+    var Mana=top.combats_plugins_manager.getMainFrame().document.all("Mana");
+    if (Mana) {
+      var maxT=1800/top.speed*100;
+      var T=Math.floor(maxT/top.maxMana*100);
+      if (tkMana<maxMana)
+        Mana.innerHTML = Mana.innerHTML+'(100HP/'+T+'сек.)';
+    }
+  }
 })()
