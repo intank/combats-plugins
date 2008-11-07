47cb0fd38b76d3370c12760247fbc57d344d2a51
shegeda@ukr.net <shegeda@ukr.net@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2008-11-07 09:15:48 +0000

    functions.html: configuration was integrated to site's design chat_speed: plugin was made configurable run_to_location: icon and favorite locations button were resized to common bounds.

--- trunk/chat_speed/plugin.js
+++ trunk/chat_speed/plugin.js
@@ -1,5 +1,37 @@
 (function() {
-  top.ChatDelay = 10;
-  top.ChatNormDelay = 10;
-  return null;
+  return {
+    ChatDelay: 10,
+    load: function(key,def_val){
+      return external.m2_readIni(combats_plugins_manager.security_id,"Combats.RU","chat_speed\\settings.ini",top.getCookie('battle'),key,def_val);
+    },
+    save: function(key,val){
+      external.m2_writeIni(combats_plugins_manager.security_id,"Combats.RU","chat_speed\\settings.ini",top.getCookie('battle'),key,val);
+    },
+    toString: function() {
+      return "Скорость обновления чата";
+    },
+    getProperties: function() {
+      return [
+        {'name':'Период обновления чата', 'value':this.ChatDelay}
+      ];
+    },
+    apply: function(a) {
+      this.ChatDelay = a[0].value;
+      if (!this.ChatDelay || this.ChatDelay<5)
+        this.ChatDelay = 5;
+      if (top.ChatDelay == top.ChatNormDelay) {
+        top.ChatNormDelay = this.ChatDelay;
+      } else {
+        top.ChatDelay = top.ChatNormDelay = this.ChatDelay;
+      }
+    },
+    setProperties: function(a) {
+      this.apply(a);
+      this.save('ChatDelay', this.ChatDelay);
+    },
+    Init: function() {
+      this.apply([{value: parseFloat(this.load('ChatDelay', '10'))}]);
+      return this;
+    }
+  }.Init();
 })()
