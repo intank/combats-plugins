6a47cf1e4ba5a97948a07bac1b12590f25de6043
shegeda@ukr.net <shegeda@ukr.net@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2008-11-13 19:09:33 +0000

    functions apply in ie8 only

--- trunk/chat_fix_ie8/plugin.js
+++ trunk/chat_fix_ie8/plugin.js
@@ -4,15 +4,17 @@
       return "Фикс бага БК в списке игроков в IE8";
     },
     Init: function() {
-//      top.frames['activeusers'].wu = 
-      top.frames['activeusers'].eval(
-	external.readFile(
-	  combats_plugins_manager.security_id,
-	  "Combats.RU",
-	  "chat_fix_ie8\\activeusers.js"
-	)
-      );
-      return this;
+      if (navigator.userAgent.indexOf('Trident/4.0;')>=0) {
+	top.frames['activeusers'].eval(
+	  external.readFile(
+	    combats_plugins_manager.security_id,
+	    "Combats.RU",
+	    "chat_fix_ie8\\activeusers.js"
+	  )
+	);
+	return this;
+      } else
+	return null;
     }
   }.Init();
 })()
