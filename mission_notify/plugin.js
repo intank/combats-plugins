8542cb0246b9d52178fd654cc9978f05449c54a6
shegeda <shegeda@ukr.net>
2008-11-25 15:58:22 +0000

    fixed bug with next mission timer renew

--- mission_notify/plugin.js
+++ mission_notify/plugin.js
@@ -1,21 +1,15 @@
 (function() {
-  function plugin_mission_notify() {
-    top.combats_plugins_manager.attachEvent(
-      'mainframe.load',
-      top.combats_plugins_manager.get_binded_method(this,this.onloadHandler));
-  }
-
-  plugin_mission_notify.prototype = {
+  return {
     toString: function() {
       return "Напоминание о доступном задании в подземельях";
     },
     onloadHandler: function() {
-      if (top.combats_plugins_manager.getMainFrame().location.pathname!='/dungeon.pl')
+      var d = top.combats_plugins_manager.getMainFrame().document;
+      if (d.location.pathname!='/dungeon.pl')
         return;
       try {
-        var elements = top.combats_plugins_manager.getMainFrame().document.getElementsByTagName('font');
-        for(var i=0; i<elements.length; i++ ) {
-          var match = elements[i].innerText.match(/Вы не можете получить задание еще\s*(?:(\d+) ч\.)?\s*(?:(\d+) мин\.)?/);
+        if (/(?:\?|&)ql=.*?/.test(d.location.search)) {
+          var match = d.body.innerText.match(/Задания отсюда недоступны еще\s*(?:(\d+) ч\.)?\s*(?:(\d+) мин\.)?/);
           if (match) {
             var notify_handler = top.combats_plugins_manager.plugins_list['notify_handler'];
             var timespan = 0;
@@ -23,8 +17,11 @@
             timespan += (match[2]==''?0:parseInt(match[2]));
             match = top.location.host.match(/^(.*?)\./);
             notify_handler.add_notification('mission','До получения задания в '+match[1],parseInt((new Date()).getTime()/60000)+timespan);
-            break;
+            return;
           }
+        }
+        var elements = d.getElementsByTagName('font');
+        for(var i=0; i<elements.length; i++ ) {
           if (elements[i].innerText == 'Вы получили новое задание.') {
             var notify_handler = top.combats_plugins_manager.plugins_list['notify_handler'];
             match = top.location.host.match(/^(.*?)\./);
@@ -35,8 +32,12 @@
       } catch (e) {
         combats_plugins_manager.logError(this,e);
       }
+    },
+    Init: function() {
+      top.combats_plugins_manager.attachEvent(
+        'mainframe.load',
+        top.combats_plugins_manager.get_binded_method(this,this.onloadHandler));
+      return this;
     }
-  };
-
-  return new plugin_mission_notify();
+  }.Init();
 })()
