1eee51715de8bc0822b8d94fda5e28862b238e91
shegeda <shegeda@ukr.net>
2009-02-02 21:51:30 +0000

    selection from multiple check lists was implemented

--- digger_check/plugin.js
+++ digger_check/plugin.js
@@ -1,7 +1,10 @@
 (function(){
   return {
-//    URL: 'http://www.bktorg.ru/list?search=',
-    URL: 'http://www.darklaw.ru/diggers.php?act=fast_check&mini_view=1&login=',
+    URL: '',
+    knownDiggersLists: {
+      BKTorg: 'http://www.bktorg.ru/list?search=',
+      DarkLaw: 'http://www.darklaw.ru/diggers.php?act=fast_check&mini_view=1&login='
+    },
     oPanel: null,
     oWindow: null,
     div: null,
@@ -16,8 +19,15 @@
         oPanel = this.oPanel = combats_plugins_manager.createWindow("Проверка диггера", 640,480);
 
         this.div = document.createElement('<div style="width:100%; height:100%;">');
-        this.div.innerHTML = '<table style="width:100%; height:100%;"><tr><td><form><input type="text"/><input type="button" value="Проверить"/></form><tr><td style="width:100%; height:100%;"><iframe style="width:100%; height:100%;"></iframe></table>';
+        this.div.innerHTML = '<table style="width:100%; height:100%;"><tr><td><form><input type="text"/><select></select><input type="button" value="Проверить"/></form><tr><td style="width:100%; height:100%;"><iframe style="width:100%; height:100%;"></iframe></table>';
         this.div.firstChild.cells[0].firstChild.onsubmit = this.div.firstChild.cells[0].firstChild.lastChild.onclick = combats_plugins_manager.get_binded_method(this,this.checkNick);
+        var select = this.div.firstChild.cells[0].firstChild.firstChild.nextSibling;
+        for(var listId in this.knownDiggersLists) {
+          var option = document.createElement('option');
+          option.innerText = listId;
+          option.selected = (this.knownDiggersLists[listId] == this.URL);
+          select.options.add(option);
+        }
         document.body.appendChild(this.div);
 	this.iframe = this.div.firstChild.cells[1].firstChild;
         oPanel.oWindow.Insert( this.div );
@@ -28,7 +38,14 @@
         this.div.firstChild.cells[0].firstChild.firstChild.value = login;
       else
         login = this.div.firstChild.cells[0].firstChild.firstChild.value;
-      oPanel.oWindow.Show( );
+      oPanel.oWindow.Show();
+      var select = this.div.firstChild.cells[0].firstChild.firstChild.nextSibling;
+      var listId = select.options[select.selectedIndex].innerText;
+      if (this.URL!=this.knownDiggersLists[listId]) {
+        this.URL = this.knownDiggersLists[listId];
+        this.configurator.saveIni('listId',listId);
+      }
+
       this.iframe.src = this.URL+login+"&"+Math.random();
       return false;
     },
@@ -43,6 +60,8 @@
       this.menuItem.innerText = 'Проверить диггера';
     },
     Init: function() {
+      this.configurator = top.combats_plugins_manager.createConfigurationElement('digger_check');
+      this.URL = this.knownDiggersLists[this.configurator.loadIni('listId','')];
       top.combats_plugins_manager.attachEvent('fighterContextMenu', 
         top.combats_plugins_manager.get_binded_method(this, this.handlerCtxMenu));
       return this;
