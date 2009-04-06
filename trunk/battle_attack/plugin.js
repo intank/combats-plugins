328de12ae18984b1ac3a2e412eb071f0ac5ddba6
shegeda@ukr.net <shegeda@ukr.net@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2009-04-06 09:55:52 +0000

    attack was enabled. battle out when finished. minor optimization was made

--- trunk/battle_attack/plugin.js
+++ trunk/battle_attack/plugin.js
@@ -68,23 +68,28 @@
     doAttack: function() {
       if (!top.Battle.bInBattle || !top.Battle.oBattle)
         return;
+      var oBattle = top.Battle.oBattle;
+      if (oBattle.bGameOver) {
+      	return top.Battle.End(oBattle.sLocation);
+      }
+
       var block = (this.block==0)?Math.floor(Math.random()*5):this.block;
-      top.Battle.oBattle.SetDefend(block, 0);
-      var defCount = top.Battle.oBattle.oAttack.rows[ 1 ].cells[ 2 ].lastChild.innerText.split(/(,| и )/).length;
+      oBattle.SetDefend(block, 0);
+      var defCount = oBattle.oAttack.rows[ 1 ].cells[ 2 ].lastChild.innerText.split(/(,| и )/).length;
       var usedBlock = {};
       for(var i=0; i<defCount; i++)
         usedBlock[(block+i)%5] = false;
 
       switch (this.attack) {
         case 0: { // случайные атаки
-          for(var i=0; i<top.Battle.oBattle.arrAttack.length; i++) {
+          for(var i=0; i<oBattle.arrAttack.length; i++) {
             while(true) {
               var attack = Math.floor(Math.random()*5);
               if (this.match==0 || !usedBlock[attack])
                 break;
             }
             usedBlock[attack] = true;
-            top.Battle.oBattle.SetAttack(attack, i);
+            oBattle.SetAttack(attack, i);
           }
           break;
         }
@@ -95,21 +100,21 @@
               break;
           }
           usedBlock[attack] = true;
-          top.Battle.oBattle.SetAttack(attack, 0);
+          oBattle.SetAttack(attack, 0);
 
-          for(var i=1; i<top.Battle.oBattle.arrAttack.length; i++) {
+          for(var i=1; i<oBattle.arrAttack.length; i++) {
             while(true) {
               var attack = Math.floor(Math.random()*5);
               if (this.match==0 || !usedBlock[attack])
                 break;
             }
             usedBlock[attack] = true;
-            top.Battle.oBattle.SetAttack(attack, i);
+            oBattle.SetAttack(attack, i);
           }
           break;
         }
         case 2: {
-          for(var i=0; i<top.Battle.oBattle.arrAttack.length; i++) {
+          for(var i=0; i<oBattle.arrAttack.length; i++) {
             while(true) {
               var attack = Math.floor(Math.random()*5);
               if (!(attack in usedBlock))
@@ -118,12 +123,12 @@
                 break;
             }
             usedBlock[attack] = true;
-            top.Battle.oBattle.SetAttack(attack, i);
+            oBattle.SetAttack(attack, i);
           }
           break;
         }
         case 3: {
-          for(var i=0; i<top.Battle.oBattle.arrAttack.length; i++) {
+          for(var i=0; i<oBattle.arrAttack.length; i++) {
             while(true) {
               var attack = Math.floor(Math.random()*5);
               if (!(attack in usedBlock) && defCount>0)
@@ -132,26 +137,26 @@
                 break;
             }
             usedBlock[attack] = true;
-            top.Battle.oBattle.SetAttack(attack, i);
+            oBattle.SetAttack(attack, i);
           }
           break;
         }
         case 4: {
           attack = block;
           usedBlock[attack] = true;
-          top.Battle.oBattle.SetAttack(attack, 0);
+          oBattle.SetAttack(attack, 0);
 
-          for(var i=1; i<top.Battle.oBattle.arrAttack.length; i++) {
+          for(var i=1; i<oBattle.arrAttack.length; i++) {
             var attack = [block,(block+2)%5,(block+3)%5][i%3];
-            top.Battle.oBattle.SetAttack(attack, i);
+            oBattle.SetAttack(attack, i);
           }
           break;
         }
       }
 
-      var attackCount = top.Battle.oBattle.arrAttack.length;
+      var attackCount = oBattle.arrAttack.length;
       var attack = (this.attack==0)?Math.floor(Math.random()*5):this.block;
-//      top.Battle.oBattle.Attack();
+      oBattle.Attack();
     },
     Init: function() {
       this.configurator = combats_plugins_manager.createConfigurationElement('battle_attack');
