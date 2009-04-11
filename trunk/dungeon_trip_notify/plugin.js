8a471d3b0efc14daa84cdabc0c70199202425382
shegeda@ukr.net <shegeda@ukr.net@8fafadde-95d8-11dd-b74b-4d5b55ca5474>
2009-04-11 12:17:53 +0000

    Legion's mount notification was separated from common timer

--- trunk/dungeon_trip_notify/plugin.js
+++ trunk/dungeon_trip_notify/plugin.js
@@ -1,19 +1,13 @@
 (function() {
-  function plugin_dungeon_trip_notify() {
-    top.combats_plugins_manager.attachEvent(
-      'mainframe.load',
-      top.combats_plugins_manager.get_binded_method(this,this.onloadHandler));
-  }
-
-  plugin_dungeon_trip_notify.prototype = {
+  return {
     toString: function() {
       return "Напоминание о посещении подземелья";
     },
-    onloadHandler: function() {
-      if (top.combats_plugins_manager.getMainFrame().location.pathname!='/dungeon.pl')
+    onloadHandler: function(eventObj) {
+      if (eventObj.window.location.pathname!='/dungeon.pl')
         return;
       try {
-        var elements = top.combats_plugins_manager.getMainFrame().document.getElementsByTagName('small');
+        var elements = eventObj.window.document.getElementsByTagName('small');
         if (elements.length>0) {
           var match = elements[elements.length-1].innerText.match(/\(Вы можете посетить подземелье через\s*(?:(\d+) ч\.)?\s*(?:(\d+) мин\.)?.*?\)/);
           if (match) {
@@ -21,14 +15,22 @@
             var timespan = 0;
             timespan += (match[1]==''?0:parseInt(match[1])*60);
             timespan += (match[2]==''?0:parseInt(match[2]));
-            notify_handler.add_notification('dungeon_trip','До посещения подземелья',parseInt((new Date()).getTime()/60000)+timespan);
+            if (/<H3>Гора Легиона<\/H3>/.test(eventObj.window.document.body.innerHTML)) {
+              notify_handler.add_notification('legionMount_trip','До посещения Горы Легиона',parseInt((new Date()).getTime()/60000)+timespan);
+            } else {
+              notify_handler.add_notification('dungeon_trip','До посещения подземелья',parseInt((new Date()).getTime()/60000)+timespan);
+            }
           }
         }
       } catch (e) {
         combats_plugins_manager.logError(this,e);
       }
+    },
+    Init: function() {
+      top.combats_plugins_manager.attachEvent(
+        'mainframe.load',
+        top.combats_plugins_manager.get_binded_method(this,this.onloadHandler));
+      return this;
     }
-  };
-
-  return new plugin_dungeon_trip_notify();
+  }.Init();
 })()
